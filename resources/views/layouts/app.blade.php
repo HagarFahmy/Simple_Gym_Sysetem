6cabdd62d224c565bcacc40c753e63476f4d8b5d
laila672 <lailatariq672@gmail.com>
2022-04-12 22:32:09 +0200

    design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.18
         </div>
-        <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+        <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">GOLDEN GYM</a>.</strong> All rights
         reserved.
     </footer>
 
