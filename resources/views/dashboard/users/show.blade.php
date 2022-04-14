c8c247047075d1b24094d3938acc8a033efbc5e7
Hagar_Ayman <hagarfahmy115@gmail.com>
2022-04-14 07:12:35 +0200

    add training session show

--- resources/views/dashboard/users/show.blade.php
+++ resources/views/dashboard/users/show.blade.php
@@ -1,23 +1,27 @@
 
 @extends('layouts.app')
 @section('content')
-
-<div class="box box-primary  ">
+<div id="show">
+<div class="box box-primary   ">
   <div class="box-header with-border">
-    <h3 class="box-title"> User</h3>
+    <h3 class="box-title">{{$user->name}}</h3>
   </div>
   <!-- /.box-header -->
-  <img src="{{ $user->image_path }}" style="width: 50px;" >
+  <img src="{{ $user->image_path }}" >
 
   <div class="text-center  ">
   <ul class="list-group list-group-flush fs-5">
-    <li class="list-group-item ">{{$user->name}}</li>
-    <li class="list-group-item ">{{$user->email}}</li>
-    <li class="list-group-item ">{{$user->gender}}</li>
-    <li class="list-group-item ">{{$user->date_of_birth}}</li>
-    <li class="list-group-item ">{{$user->gym->name}}</li>
+    <li class="list-group-item "><span>Email : </span>{{$user->email}}</li>
+    <li class="list-group-item "><span>Gender : </span>{{$user->gender}}</li>
+    <li class="list-group-item "><span>Date Of Birth :</span>{{$user->date_of_birth}}</li>
+    <li class="list-group-item "><span>Gym Name : </span>{{$user->gym->name}}</li>
+    <li><h3>Training Sessions</h3></li>
+    @foreach($user->training_sessions as $training_session)
+    <li>{{$training_session->name}}</li>
+    @endforeach
   </ul>
   </div>
 
+</div>
 </div>
 @endsection
