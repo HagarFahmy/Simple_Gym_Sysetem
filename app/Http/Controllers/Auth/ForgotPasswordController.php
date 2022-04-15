810b6a175bec217722130455818233a4a2dcaadb
laila672 <lailatariq672@gmail.com>
2022-04-16 00:21:05 +0200

    forget & reset password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +20,33 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+//     protected function guard()
+//     {
+//         return Auth::guard('admin');
+//     }
+//     protected function broker()
+// {
+//     return Password::broker('admins');
+// }
+
+
+// public function showLinkRequestForm()
+// {
+//     return view('auth.passwords.email');
+// }
+
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+    public function broker()
+    {
+        return Password::broker('admins');
+    }
 }
+
